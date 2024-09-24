2fc3a1a5a766b103bbdbd86be1d167cc3535f0b3
Dmitriy267 <dmitrio87@mail.ru>
2024-09-24 22:27:15 +0300

    [branchFerst] решение проблемы с cors

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,18 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: 'https://github.com/Dmitriy267/momentum',
+    base: 'https://github.com/Dmitriy267/',
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3000',
+                changeOrigin: true,
+                configure: (proxy, options) => {
+                    proxy.on('proxyReq', (proxyReq, req, res) => {
+                        proxyReq.setHeader('Origin', 'http://localhost:3000');
+                    });
+                },
+            },
+        },
+    },
 });
