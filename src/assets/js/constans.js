004888a12cf76cf5bb831e670e37bce278191e86
Dmitriy267 <dmitrio87@mail.ru>
2024-09-26 02:16:46 +0300

    [branchFerst]

--- src/assets/js/constans.js
+++ src/assets/js/constans.js
@@ -1,6 +1,6 @@
 export const data = new Date();
 
-export const arr = ['/01.jpg', '/02.jpg', '/03.jpg', '/04.jpg'];
+export const arr = ['01.jpg', '02.jpg', '03.jpg', '04.jpg'];
 
 export const div = document.querySelector('#image');
 export const hour = document.querySelector('#hour');
